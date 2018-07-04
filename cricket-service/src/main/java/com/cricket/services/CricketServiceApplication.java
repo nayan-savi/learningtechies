3c0764e57d043be4fc7b4f009896cb84e79b9ddd
nayansavi <nayan.savi@gmail.com>
2018-07-05 02:08:20 +0530

    initial code for cricket service

---
+++ cricket-service/src/main/java/com/cricket/services/CricketServiceApplication.java
@@ -0,0 +1,12 @@
+package com.cricket.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketServiceApplication.class, args);
+	}
+}
